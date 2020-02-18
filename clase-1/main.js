0fc4be0e97431404c86913b3230d3d14de40a81d
JoaMoreno <tec.ignaciomoreno@gmail.com>
2020-02-18 18:22:46 -0300

    clase-2

--- clase-1/main.js
+++ clase-1/main.js
@@ -1,7 +1,16 @@
-import {clasesFunction} from "./js/class-function.js"
-
 var mainContainer = document.getElementById("main-container");
 
-//var toggle = document.getElementById("toggle-lateral");
-//toggle.addEventListener("click", clasesFunction.toggleBetweenAreas("main-area-1","main-area-2"));
+isToggle = false;
+    toggleBetweenAreas = () => {
+        if (isToggle) {
+            mainContainer.classList.replace("main-area-2", "main-area-1")
+            this.isToggle = false;
+        } else {
+            mainContainer.classList.replace("main-area-1", "main-area-2")
+            this.isToggle = true;
+        }
+    }
+
+var toggle = document.getElementById("toggle-lateral");
+toggle.addEventListener("click", toggleBetweenAreas);
 
